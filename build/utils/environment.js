38b905c4c2f42360625fbf25c81ea0f43a9126b0
liudan185 <liudan185@jd.com>
2020-08-13 21:15:06 +0800

    ci: 使用akfun前端脚手架

--- build/utils/environment.js
+++
@@ -1,4 +0,0 @@
-const isDevelopmentEnv = () => process.env.NODE_ENV === 'development';
-const isProductionEnv = () => process.env.NODE_ENV === 'production';
-
-export { isDevelopmentEnv, isProductionEnv };
